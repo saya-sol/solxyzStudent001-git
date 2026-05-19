94c511377af45fe51206102789dab033ff0e7af7
PC80189-PC <PC80189-PC@PC80189>
2026-05-19 16:11:42 +0900

    さや

--- src/p01/Test01.java
+++ src/p01/Test01.java
@@ -13,6 +13,7 @@ public class Test01 {
 		System.out.println("Hi");
 		System.out.println("Hello!");
 		System.out.println("GG");
+
 	}
 
 }
---
+++ src/p01/sayadayo.java
@@ -0,0 +1,10 @@
+package p01;
+
+public class sayadayo {
+	public static void main(String[] args) {
+		String name = "saya";
+		if (name.equals("saya")) {
+			System.out.println("大森紗弥です！");
+		}
+	}
+}
